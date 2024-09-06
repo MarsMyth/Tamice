bb867aa86eaa01df922ec3feb85bc35a9a240450
MarsMyth <callumshelton2503@gmail.com>
2024-09-07 07:07:52 +1000

    Original Commit

---
+++ src/main/java/net/mars_myth/tamice/TotallyNotAHorrorMod.java
@@ -0,0 +1,24 @@
+package net.mars_myth.tamice;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.kyrptonaught.customportalapi.api.CustomPortalBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TotallyNotAHorrorMod implements ModInitializer {
+	public static final String MOD_ID = "horror-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("This is not a horror game");
+
+
+	}
+}
---
+++ src/main/java/net/mars_myth/tamice/TotallyNotAHorrorModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mars_myth.tamice;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TotallyNotAHorrorModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
