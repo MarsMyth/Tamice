84617d9604e87a8094b81fc0a2e0b584f89c78a3
MarsMyth <callumshelton2503@gmail.com>
2024-09-07 15:29:17 +1000

    Fixed Datagen

--- src/main/java/net/mars_myth/tamice/TotallyNotAHorrorModDataGenerator.java
+++ src/main/java/net/mars_myth/tamice/TotallyNotAHorrorModDataGenerator.java
@@ -2,10 +2,13 @@ package net.mars_myth.tamice;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.mars_myth.tamice.datagen.ModModelProvider;
 
 public class TotallyNotAHorrorModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
